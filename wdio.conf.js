b0a1ac2dc82c95f315f3f719185ba00a5380b9b4
Pradap Pandiyan <pradapjackie@gmail.com>
2025-02-16 22:44:11 +0400

    first commit to run on the circle ci

---
+++ wdio.conf.js
@@ -0,0 +1,24 @@
+exports.config = {
+    runner: 'local',
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    exclude: [],
+    maxInstances: 10,
+    capabilities: [{
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['--headless', '--disable-gpu', '--window-size=1280,800']
+        }
+    }],
+    logLevel: 'info',
+    bail: 0,
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    }
+};
